ede5386f289305a67788ac501746806359eddbbd
DanaFo <x.dfortier@gmail.com>
2023-11-22 11:48:24 -0800

    initial setup for c++ and gas

--- Source/Test/Test.Build.cs
+++ Source/Test/Test.Build.cs
@@ -8,6 +8,11 @@ public class Test : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "AIModule", "Niagara", "EnhancedInput" });
-    }
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "AIModule", "Niagara", "EnhancedInput",
+			"UMG", "GameplayAbilities", "GameplayTags", "GameplayTasks"
+		});
+
+	}
 }
